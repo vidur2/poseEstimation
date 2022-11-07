d7c70b2090912100efaef09873e7d8c223ac614b
vidur2 <vmod2005@gmail.com>
2022-11-07 10:08:59 -0500

    better

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import apriltag_video
 from networktables import NetworkTables, NetworkTablesInstance
 
 funcPaths = ["./t_x.pickle", "./t_y.pickle", "./x_t.pickle", "./y_t.pickle"]
+matrixPath = ["./aprilPose.pickle"]
 err = 0.5
 
 NetworkTables.initialize(server="10.51.09.2")
@@ -30,16 +31,21 @@ def main():
             obj = loads(codecs.decode(f.read(), "base64"))
             funcsParsed[func] = obj
 
+    offset = None
+    with open(matrixPath, 'rb') as f:
+        offset = loads(codecs.decode(f.read(), "base64"))
+
     # alignToStart()
     for res in apriltag_video.apriltag_video(input_streams=[0]):
         if (len(res)):
-            poses = res[1]
-            center = np.zeros(4)
-            for pose in poses:
-                center += pose
-            center = center/4
-            tEq = splev(center[0], funcsParsed["./t_x.pickle"]["self"])
-            tVer = splev(center[4], funcsParsed["./t_y.pickle"]["self"])
+            pose = res[1]
+            yVal = pose[2][3]
+            xVal = pose[0][3]
+
+            # TODO multpily by transformation here
+            # print((wxVal, yVal))  
+            tEq = splev(xVal, funcsParsed["./t_x.pickle"]["self"])
+            tVer = splev(yVal, funcsParsed["./t_y.pickle"]["self"])
             xSpeed= splev(tEq, funcsParsed["./x_t.pickle"]["diff"])
             ySpeed = splev(tVer, funcsParsed["./y_t.pickle"]["diff"])
 
