fde9146aeb6d1058070e37aa6bb30454ff5e9ad7
vidur2 <vmod2005@gmail.com>
2022-11-20 15:03:31 -0500

    better testing?

--- tank_test.py
+++ tank_test.py
@@ -6,8 +6,14 @@ def test():
     lMotor = Motor(16, 25, 12, False)
     rMotor = Motor(5, 6, 13, True)
 
-    lMotor.forward(lPower)
-    rMotor.forward(rPower)
+    lMotor.forward(0.25)
+    rMotor.forward(0.25)
     sleep(2)
     rMotor.forward(0)
     lMotor.forward(0)
+
+def velTest():
+    pass
+
+if __name__ == '__main__':
+    test()
