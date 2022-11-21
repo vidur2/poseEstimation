bd8dd331b712814ed4f2d2577d60393cd1fcf15b
vidur2 <vmod2005@gmail.com>
2022-11-21 11:03:17 -0500

    better?

--- controller.py
+++ controller.py
@@ -2,11 +2,12 @@ from motor import Motor
 from encoder import Encoder
 
 class Overspeed:
-    def __init__(self, motor: Motor, encoder: Encoder, desiredVel: int, maxPow: int):
+    def __init__(self, motor: Motor, encoder: Encoder, desiredVel: int, maxPow: int=1, minPow: int=0):
         self.motor = motor
         self.encoder = encoder
         self.desiredVel = desiredVel
         self.maxPow = maxPow
+        self.minPow = minPow
     
     def setDesiredVel(self, vel: int):
         self.desiredVel = desiredVel
